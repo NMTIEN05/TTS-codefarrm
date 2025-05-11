4874525e769c890f2b72e79a3a2b00bfba1cea54
NMTIEN05 <nmtbao001@gmail.com>
2025-05-11 13:33:07 +0700

    first commit

---
+++ fe/src/App.tsx
@@ -0,0 +1,14 @@
+import { Layout, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
+import { Link } from "react-router-dom";
+import "antd/dist/reset.css";
+
+function App() {
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+     <div>aaaaaaaa</div>
+    </Layout>
+  );
+}
+
+export default App;
